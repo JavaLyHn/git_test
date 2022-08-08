72fe135046d00bcd39e7343fc757b9b201962879
LyHN <LyHN@LyHN.com>
2022-08-08 22:11:20 +0800

    first commit

---
+++ src/main/java/com/LyHN/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.LyHN.git;
+
+/**
+ * @author 陆昊男
+ * @version 1.0
+ * @description: TODO
+ * @date 2022/8/8 22:01
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
